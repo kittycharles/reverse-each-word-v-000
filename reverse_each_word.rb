58ea075e9fe2350aa2f068a0d8861986a628756d
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-26 22:02:57 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(sentence)
   a_sentence = sentence.split
   a_sentence.collect do |reversed_sentence|
     reversed_array = reversed_sentence.reverse
+    reversed_array.join
   end
 
 end
