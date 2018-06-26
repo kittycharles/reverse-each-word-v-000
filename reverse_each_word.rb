a1ee668be6a2dc96bc3000058ce6db3bbb0d5104
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-26 22:14:21 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(sentence)
-  a_sentence = sentence.split
-  a_sentence.map do |reversed_sentence|
+  a_sentence = sentence.split(" ").map do |reversed_sentence|
     reversed_sentence.reverse
   end
-
+a_sentence.join(" ")
 end
